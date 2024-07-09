2a29f28fcb8d022bae87e27f4589d77b742b8b17
ch0ls <jonunez2019@udec.cl>
2024-07-09 09:30:01 -0400

    cambios estructura frame

--- callbacks.py
+++ callbacks.py
@@ -3,16 +3,22 @@ from graphs.teamprogress_linegraph import prepare_data_and_fig
 
 def register_callbacks(app):
     @app.callback(
-        Output('output-1', 'children'),
-        Output('output-2', 'children'),
-        Output('main-graph', 'figure'),
+        Output('graph-1', 'figure'),
+        Output('graph-2', 'figure'),
+        Output('graph-3', 'figure'),
+        Output('graph-4', 'figure'),
+        Output('player-1', 'children'),
+        Output('player-2', 'children'),
+        Output('player-3', 'children'),
+        Output('player-4', 'children'),
         Input({'type': 'team-button', 'index': ALL}, 'n_clicks')
     )
     def update_dashboard(n_clicks):
         ctx = dash.callback_context
         if not ctx.triggered:
-            return "Output 1: No team selected", "Output 2: No team selected", prepare_data_and_fig()
+            return prepare_data_and_fig(), prepare_data_and_fig(), prepare_data_and_fig(), prepare_data_and_fig(), "Player 1", "Player 2", "Player 3", "Player 4"
         else:
             button_id = ctx.triggered[0]['prop_id'].split('.')[0]
             team = eval(button_id)['index']
-            return f"Output 1: {team}", f"Output 2: Selected Value: {team}", prepare_data_and_fig(team)
+            # Aquí deberás agregar la lógica para obtener los gráficos y jugadores del equipo seleccionado
+            return prepare_data_and_fig(team), prepare_data_and_fig(team), prepare_data_and_fig(team), prepare_data_and_fig(team), f"Player 1 of {team}", f"Player 2 of {team}", f"Player 3 of {team}", f"Player 4 of {team}"
--- layout.py
+++ layout.py
@@ -1,14 +1,16 @@
 from dash import html, dcc
 import dash_bootstrap_components as dbc
 
-def create_layout(app, teams, fig):
+def create_layout(teams):
     team_buttons = [
-        dbc.Button(html.Img(src=f'assets/teams/{team.lower().replace(" ", "")}.png', style={'height': '80%', 'width': 'auto'}), 
+        dbc.Button(html.Img(src=f'assets/teams/{team.lower().replace(" ", "")}.png', style={'height': '100%', 'width': 'auto'}), 
                    id={'type': 'team-button', 'index': team}, className="btn btn-info", style={'width': '70px', 'height': '70px', 'padding': '5px'}, n_clicks=0)
         for team in teams
     ]
 
-    return dbc.Container([
+    return html.Div([
+        
+        dbc.Container([
         html.Div(
             [
                 html.Div(
@@ -27,18 +29,61 @@ def create_layout(app, teams, fig):
                     style={'margin-left': 15, 'margin-right': 15, 'display': 'flex', 'flex-wrap': 'wrap'}
                 ),
             ],
-            style={'width': 400, 'margin-left': 35, 'margin-top': 35, 'margin-bottom': 35}
+            style={'width': '20%', 'margin-left': 35, 'margin-right': 35, 'margin-top': 35, 'margin-bottom': 35}
         ),
         html.Div(
             [
-                html.Div(dcc.Graph(id='main-graph', figure=fig), style={'width': 790}),
+                # Gráficos 
                 html.Div([
-                    html.H2('Output 1:'),
-                    html.Div(id='output-1', className='Output'),
-                    html.H2('Output 2:'),
-                    html.Div(id='output-2', className='Output')
-                ], style={'width': 200})
+                    html.Div(dcc.Graph(id='graph-1'), className='graph-square', style={'height': '50%', 'width': '100%'}),
+                ]),
+                html.Div([
+                    html.Div(dcc.Graph(id='graph-2'), className='graph-square', style={'height': '50%', 'width': '100%'}),
+                ]),
+                html.Div([
+                    html.Div(dcc.Graph(id='graph-3'), className='graph-square', style={'height': '50%', 'width': '100%'}),
+                ]),
+                html.Div([
+                    html.Div(dcc.Graph(id='graph-4'), className='graph-square', style={'height': '50%', 'width': '100%'}),
+                ])
             ],
-            style={'width': 990, 'margin-top': 35, 'margin-right': 35, 'margin-bottom': 35, 'display': 'flex'}
+            style={'overflowY': 'scroll', 'padding': '20px', 'flex': '0 0 56%'}
+        ),
+        html.Div(
+            [
+                # Lista de jugadores en la sección de la derecha con barra de desplazamiento
+                html.Div([
+                    html.Div(id='player-1', className='player-info'),
+                    html.Div(id='player-2', className='player-info'),
+                    html.Div(id='player-3', className='player-info'),
+                    html.Div(id='player-4', className='player-info'),
+                    html.Div(id='player-1', className='player-info'),
+                    html.Div(id='player-2', className='player-info'),
+                    html.Div(id='player-3', className='player-info'),
+                    html.Div(id='player-4', className='player-info'),
+                    html.Div(id='player-1', className='player-info'),
+                    html.Div(id='player-2', className='player-info'),
+                    html.Div(id='player-3', className='player-info'),
+                    html.Div(id='player-4', className='player-info'),
+                    html.Div(id='player-1', className='player-info'),
+                    html.Div(id='player-2', className='player-info'),
+                    html.Div(id='player-3', className='player-info'),
+                    html.Div(id='player-4', className='player-info'),
+                    html.Div(id='player-1', className='player-info'),
+                    html.Div(id='player-2', className='player-info'),
+                    html.Div(id='player-3', className='player-info'),
+                    html.Div(id='player-4', className='player-info'),
+                    html.Div(id='player-1', className='player-info'),
+                    html.Div(id='player-2', className='player-info'),
+                    html.Div(id='player-3', className='player-info'),
+                    html.Div(id='player-4', className='player-info')
+                ],
+            style={'width': '100%','height': '100%'})
+            ],
+            style={'overflowY': 'scroll','overflowX': 'hidden','padding': '15px', 'flex': '0 0 20%', 'background-color': '#333', 'color': '#fff'}
         )
-    ], fluid=True, style={'display': 'flex'}, className='dashboard-container')
+    ], fluid=True, style={'display': 'flex', 'flex-direction': 'row',}, className='dashboard-container')
+    ],
+      style={  }
+                    )
+    
