42268c260ad9bd63d65fd328451fd9b7fd57b901
Lizandro Ruiz <111071454+LizandroR@users.noreply.github.com>
2024-07-09 15:13:34 -0400

    Update performance_evolution.py
    documentacion

--- graphs/performance_evolution.py
+++ graphs/performance_evolution.py
@@ -8,17 +8,15 @@ def prepare_data_and_fig_evolution(team_name):
     matches_df = pd.read_csv(
         './data/england-premier-league-matches-2018-to-2019-stats.csv')
 
-    # Filtra los partidos en los que el equipo seleccionado jugó como local o visitante
     # Filtra los partidos en los que el equipo seleccionado jugó como local o visitante.
     team_matches = matches_df[(matches_df['home_team_name'] == team_name) | (
         matches_df['away_team_name'] == team_name)].copy()
 
     # Convierte la columna de fecha a formato datetime
-    # Convierte la columna de fecha a formato datetime
     team_matches['date_GMT'] = pd.to_datetime(
         team_matches['date_GMT'], format='%b %d %Y - %I:%M%p')
 
-    #  Calcula los puntos obtenidos en cada partido.
+    
     #  Calcula los puntos obtenidos en cada partido.
     def calculate_points(row):
         if row['home_team_name'] == team_name:
@@ -35,26 +33,26 @@ def prepare_data_and_fig_evolution(team_name):
                 return 1  # empate
             else:
                 return 0  # loss
-    # Aplicar la función de cálculo de puntos a cada fila.
+    .
                 return 0  # derrota
             
      # Aplicar la función de cálculo de puntos a cada fila.
     team_matches['points_per_game'] = team_matches.apply(
         calculate_points, axis=1)
 
-      # Calcular goles anotados y recibidos dependiendo si el equipo juega local o fuera.
+     
      # Calcular goles anotados y recibidos dependiendo si el equipo juega local o fuera.
     team_matches['goals_scored'] = team_matches.apply(
         lambda x: x['home_team_goal_count'] if x['home_team_name'] == team_name else x['away_team_goal_count'], axis=1)
     team_matches['goals_conceded'] = team_matches.apply(
         lambda x: x['away_team_goal_count'] if x['home_team_name'] == team_name else x['home_team_goal_count'], axis=1)
 
-    # Crear un layout de subplots: 2 filas, 1 columna, ejes X compartidos.
+    
     # rear un layout de subplots: 2 filas, 1 columna, ejes X compartidos.
     fig = make_subplots(rows=2, cols=1, shared_xaxes=True,
                         vertical_spacing=0.1)
 
-    # Agrega un gráfico de líneas para los puntos por partido en la primera fila.
+    
     # Agrega un gráfico de líneas para los puntos por partido en la primera fila.
     fig.add_trace(
         go.Scatter(x=team_matches['date_GMT'], y=team_matches['points_per_game'],
@@ -62,7 +60,7 @@ def prepare_data_and_fig_evolution(team_name):
         row=1, col=1
     )
 
-    # Agrega un gráfico de líneas para los goles anotados en la segunda fila.
+    
     # Agrega un gráfico de líneas para los goles anotados en la segunda fila.
     fig.add_trace(
         go.Scatter(x=team_matches['date_GMT'], y=team_matches['goals_scored'],
@@ -70,7 +68,7 @@ def prepare_data_and_fig_evolution(team_name):
         row=2, col=1
     )
 
-   # Agregar un gráfico de líneas para los goles concedidos en la segunda fila.
+   
     # Agregar un gráfico de líneas para los goles concedidos en la segunda fila.
     fig.add_trace(
         go.Scatter(x=team_matches['date_GMT'], y=team_matches['goals_conceded'],
